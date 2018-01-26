4fb83e613a699acf97f07af7f4b8c395ef67bb0f
Thiago Monteiro Barbosa <supermonteiro@gmail.com>
2018-01-26 06:04:00 -0400

    1.0
    *Connection information provided through options screen
    *Read whitelist from external json file, example included
    *Sends chrome user's email to the LRS
    *Improved the design of the options screen

--- bg.js
+++ bg.js
@@ -1,6 +1,8 @@
 var prefs = {};
+var whitelist_data = [];
+var userName, userID="";
 
-chrome.storage.local.get({endpoint: 'http://localhost:8080', username: 'chrome', secret: 'arthas'}, function(o) { prefs = o; });
+chrome.storage.local.get({endpoint: 'http://localhost:8080', username: 'your-username', secret: 'your-secret'}, function(o) { prefs = o; });
 
 chrome.storage.onChanged.addListener(function(changes) {
     for (key in changes) {
@@ -8,65 +10,83 @@ chrome.storage.onChanged.addListener(function(changes) {
     }
 });
 
-var whitelist = ["http://www.cgs.com", "http://www.cbc.ca", "http://ec2-54-177-217-147.us-west-1.compute.amazonaws.com/organisation/5a472b2104d8cd0e9d45f61f/data/source", "chrome://newtab/", "chrome://extensions/"];
+var whitelist = [];
+
+chrome.storage.local.get('newArr', function (result) {
+    whitelist = result.newArr;        
+    for (i = 0; i < whitelist.list.length; i++){
+        whitelist_data.push(whitelist.list[i]);
+    }
+});
+
+chrome.identity.getProfileUserInfo(function(userInfo){userName= userInfo.email});
+chrome.identity.getProfileUserInfo(function(userInfo){userID= userInfo.id});
+
 
 function log(url, title, favicon){
-    /*var data = JSON.stringify({
-        url: url, time: Date.now(),
-        title: title, key: prefs.key,
-        favicon: favicon
-    });
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", prefs.callback);
-    xhr.send(data);*/
-    console.log(url, title);
-    activityInfo = {};
-    var checkWhitelist = whitelist.indexOf(url);
-    if (checkWhitelist == -1 && url != null) {
-        var tincan = new TinCan (
-        {
-            recordStores: [
+    if (url != null) {
+        activityInfo = {};
+        if (typeof whitelist != 'undefined') {
+            //whitelist.toString();
+            var checkWhitelist = whitelist_data.indexOf(url);            
+            if (checkWhitelist == -1) {
+                var tincan = new TinCan (
+            /*  {
+                    recordStores: [
+                        {
+                            endpoint: "http://ec2-54-177-217-147.us-west-1.compute.amazonaws.com/data/xAPI",
+                            username: "6ee5500aa766a72acf309706ead0c78aca868a72",
+                            password: "ce96135d94f9cb18c10344dc3f10fce94d39a03b",
+                            allowFail: false
+                        }
+                    ]
+                }); */
+                    
                 {
-                    endpoint: "http://ec2-54-177-217-147.us-west-1.compute.amazonaws.com/data/xAPI",
-                    username: "6ee5500aa766a72acf309706ead0c78aca868a72",
-                    password: "ce96135d94f9cb18c10344dc3f10fce94d39a03b",
-                    allowFail: false
-                }
-            ]
-        });
-
-        tincan.sendStatement(
-        {
-            actor: {
-                mbox: "mailto:your.email.address@example.com",
-                name: "User 3"
-            },
-            verb: {
-                display: {
-                 "en-GB": "accessed"
+                    recordStores: [
+                        {
+                            endpoint: prefs.endpoint,
+                            username: prefs.username,
+                            password: prefs.secret,
+                            allowFail: false
+                        }
+                    ]
+                });    
+                
+                tincan.sendStatement(
+                {
+                    actor: {
+                        mbox: "mailto:"+userName,
+                        name: userName
+                    },
+                    verb: {
+                        display: {
+                         "en-GB": "accessed"
+                        },
+                        id: "http://adlnet.gov/expapi/verbs/accessed"
+                    },
+                    object: {
+                        id: "http://www.cgs.com",
+                        definition: {
+                            name: {
+                                "en-GB": "\""+url+"\""
+                            }
+                        }
+                   }
                 },
-                id: "http://adlnet.gov/expapi/verbs/accessed"
-            },
-            object: {
-                id: "http://www.cgs.com",
-                definition: {
-                    name: {
-                        "en-GB": "\""+url+"\""
-                    }
-                }
-           }
-        },
 
-        function (err, result) {
-            //Handle any errors here. This code just outputs the result to the page. 
-            document.write("<pre>");
-            document.write(JSON.stringify(err, null, 4));
-            document.write("</pre>");
-            document.write("<pre>");
-            document.write(JSON.stringify(result, null, 4));
-            document.write("</pre>");
-        }); 
-    }                  
+                function (err, result) {
+                    //Handle any errors here. This code just outputs the result to the page. 
+                    document.write("<pre>");
+                    document.write(JSON.stringify(err, null, 4));
+                    document.write("</pre>");
+                    document.write("<pre>");
+                    document.write(JSON.stringify(result, null, 4));
+                    document.write("</pre>");
+                }); 
+            }
+        }            
+    }                    
 }
 
 chrome.tabs.onActivated.addListener(function (activeInfo) {
--- options.js
+++ options.js
@@ -1,5 +1,5 @@
 function save() {
-    var data = {};
+    var data = {};  
     data[this.id] = this.value;
     chrome.storage.local.set(data);
 }
@@ -7,8 +7,8 @@ function save() {
 function restore_options() {
     chrome.storage.local.get({
         endpoint: 'http://localhost:8080',
-        username: 'chrome',
-        secret: 'arthas'
+        username: 'your-username',
+        secret: 'your-secret'
     }, function(options) {
         document.getElementById('endpoint').value = options.endpoint;
         document.getElementById('username').value = options.username;
@@ -16,7 +16,50 @@ function restore_options() {
     });
 }
 
+function loadFile() {
+    var input, file, fr;
+
+    if (typeof window.FileReader !== 'function') {
+      console.log("The file API isn't supported on this browser yet.");
+      return;
+    }
+
+    input = document.getElementById('fileinput');
+    if (!input) {
+      console.log("Couldn't find the fileinput element.");
+    }
+    else if (!input.files) {
+      console.log("This browser doesn't seem to support the `files` property of file inputs.");
+    }
+    else if (!input.files[0]) {
+      console.log("Please select a file before clicking 'Load'");
+    }
+    else {
+      file = input.files[0];
+      fr = new FileReader();
+      fr.onload = function(e) { 
+            console.log(e);
+            var result = JSON.parse(e.target.result);
+            chrome.storage.local.set({'newArr': result});            
+        }
+
+      fr.readAsText(file);            
+    } 
+    
+    function receivedText(e) {
+      var lines = e.target.result;
+      var newArr = JSON.parse(lines);      
+      chrome.storage.local.set({'newArr': newArr});
+    }
+  }
+
+
+
 document.getElementById('endpoint').addEventListener('change', save);
 document.getElementById('username').addEventListener('change', save);
 document.getElementById('secret').addEventListener('change', save);
 document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save);
+document.getElementById('btnLoad').addEventListener('click',
+    loadFile);
